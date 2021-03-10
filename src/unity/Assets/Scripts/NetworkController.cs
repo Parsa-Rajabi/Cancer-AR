2cd09cafa2e077fd67ecaced878a465c8a6a8ea8
Parsa <ParsaRajabiPR@gmail.com>
2021-03-10 12:00:40 -0800

    Created Network Controller Script

---
+++ src/unity/Assets/Scripts/NetworkController.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Started Photon");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Cloud Region is " + PhotonNetwork.CloudRegion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("Once per frame");
+    }
+}
